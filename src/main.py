0ed442dd00b681fb4eba68c6281aab53a7141fe4
JakeGIGABYTE <jake.chu@gigabyte.com>
2024-07-12 11:35:35 +0800

    test env null

--- src/main.py
+++ src/main.py
@@ -24,7 +24,7 @@ async def favicon():
 async def hello(request: Request, name: str = Form(...)):
     if name:
         print('Request for hello page received with name=%s' % name)
-        return templates.TemplateResponse('hello.html', {"request": request, 'name':name})
+        return templates.TemplateResponse('hello.html', {"request": request, 'name': name})
     else:
         print('Request for hello page received with no name or blank name -- redirecting')
         return RedirectResponse(request.url_for("index"), status_code=status.HTTP_302_FOUND)
